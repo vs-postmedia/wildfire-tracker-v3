9bb8d29c5a6259d6c8d9dabb7af75cb974474d03
Nathan Griffiths <ngriffiths@postmedia.com>
2023-05-03 20:36:54 -0700

    Updates

---
+++ precache-manifest.952eef26347f888f975f29c1db5dac06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc9a2a615648760d7b0304a713a52a4",
+    "url": "/wildfire-tracker-dev/index.html"
+  },
+  {
+    "revision": "572ca03a73b996e4988b",
+    "url": "/wildfire-tracker-dev/static/css/main.ca6fb90b.chunk.css"
+  },
+  {
+    "revision": "78024861aa6abf918e42",
+    "url": "/wildfire-tracker-dev/static/js/2.46936c43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wildfire-tracker-dev/static/js/2.46936c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572ca03a73b996e4988b",
+    "url": "/wildfire-tracker-dev/static/js/main.4b6dd2b7.chunk.js"
+  },
+  {
+    "revision": "77ca9749b7dbb2a3ba68",
+    "url": "/wildfire-tracker-dev/static/js/runtime-main.06b0a967.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/wildfire-tracker-dev/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/wildfire-tracker-dev/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/wildfire-tracker-dev/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/wildfire-tracker-dev/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/wildfire-tracker-dev/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/wildfire-tracker-dev/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
