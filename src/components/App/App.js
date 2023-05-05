43f9cd0f67108298d2b5d2adbb864b2273c8e544
Nathan Griffiths <ngriffiths@postmedia.com>
2023-05-05 12:16:37 -0700

    updated url for firesmoke.ca data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ const currentFiresURL = 'https://raw.githubusercontent.com/vs-postmedia/bc-wildf
 // evacuation order & alert perimeters
 const evacsAlertsUrl = 'https://services6.arcgis.com/ubm4tcTYICKBpist/arcgis/rest/services/Evacuation_Orders_and_Alerts/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=ORDER_ALERT_STATUS,SHAPE__AREA,EVENT_TYPE';
 // firesmoke png file
-const fireSmokeUrl = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/wildfires/fire-smoke-max.png';
+const fireSmokeUrl = 'https://raw.githubusercontent.com/vs-postmedia/bc-wildfire-scraper/master/data/daily-max-dispersion.png';
 
 // map tiles & attribution
 const mapboxStyle = 'https://api.maptiler.com/maps/outdoor/style.json?key=pRmETZ6APJE6l5kAzesF';
